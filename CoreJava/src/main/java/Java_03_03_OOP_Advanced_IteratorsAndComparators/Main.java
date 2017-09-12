84abeaad3d30533bac37d956713c583285249979
Ivan Ivanov <iv.ivanov@seeburger.com>
2017-09-12 15:30:55 +0300

    Removed label

--- CoreJava/src/main/java/Java_03_03_OOP_Advanced_IteratorsAndComparators/Main.java
+++ CoreJava/src/main/java/Java_03_03_OOP_Advanced_IteratorsAndComparators/Main.java
@@ -1,10 +1,3 @@
-/*
- * Main.java
- *
- * created at Sep 12, 2017 by iv.ivanov <iv.ivanov@seeburger.de>
- *
- * Copyright (c) SEEBURGER AG, Germany. All Rights Reserved.
- */
 package Java_03_03_OOP_Advanced_IteratorsAndComparators;
 
 /**
